bb6606aefabdcdc7c70a4d769e962d7e7b7663d0
zkeschner <106891094+zkeschner@users.noreply.github.com>
2023-07-23 15:41:07 -0400

    Delete bank.py

--- bank.py
+++
@@ -1,9 +0,0 @@
-inp = str(input("Greeting: "))
-inp = inp.split()
-if inp[0].lower() == "hello" or inp[0].lower() == "hello,":
-    print("$0")
-else:
-    if inp[0][0].lower() == "h":
-        print("$20")
-    else:
-        print("$100")
